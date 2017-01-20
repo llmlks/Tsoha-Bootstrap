6d5f840295a867bd92982fd4a41a41a0f80c45b2
llmlks <lamelkas@gmail.com>
2017-01-20 10:55:12 +0200

    Sovellus alustettu ja versionhallinta otettu kayttoon

--- config/environment.sh
+++ config/environment.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # korvaa tämä omalla käyttäjätunnuksellasi
-USERNAME="kalleilv"
+USERNAME="lmelkas"
 # korvaa tämä haluamallasi kansion nimellä
-PROJECT_FOLDER="tsoha"
+PROJECT_FOLDER="bandbook"
 # sovelluksesi tulee sijaitsemaan osoitteessa USERNAME.users.cs.helsinki.fi/PROJECT_FOLDER
