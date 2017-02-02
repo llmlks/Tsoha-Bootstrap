b21df19b5f7fd07e1df5dfe1864a85648b814443
llmlks <lamelkas@gmail.com>
2017-02-02 21:38:24 +0200

    search toimii

--- app/models/band.php
+++ app/models/band.php
@@ -1,21 +1,21 @@
 <?php
 
 class Band extends BaseModel {
-    
+
     public $bandname, $description, $origin, $id, $username, $password;
-    
+
     public function __construct($attributes) {
         parent::__construct($attributes);
     }
-    
+
     public static function findall() {
-        
+
         $query = DB::connection()->prepare('SELECT * FROM Band');
         $query->execute();
-        
+
         $rows = $query->fetchAll();
         $bands = array();
-        
+
         foreach ($rows as $row) {
             $bands[] = new Band(array(
                 'bandname' => $row['bandname'],
@@ -26,18 +26,18 @@ class Band extends BaseModel {
                 'password' => $row['password']
             ));
         }
-        
+
         return $bands;
     }
-    
+
     public static function findwithid($id) {
-        
+
         $query = DB::connection()->prepare('SELECT * FROM Band WHERE id = :id '
                 . 'LIMIT 1');
         $query->execute(array('id' => $id));
-        
+
         $row = $query->fetch();
-        
+
         if ($row) {
             $band = new Band(array(
                 'bandname' => $row['bandname'],
@@ -49,25 +49,26 @@ class Band extends BaseModel {
             ));
             return $band;
         }
-        
+
         return null;
     }
 
     public static function findwithname($bandname) {
-        
+
+        $bandname = strtolower($bandname);
         $bandname = '%' . $bandname . '%';
-        
-        $query = DB::connection()->prepare('SELECT * FROM Band WHERE bandname = :bandname');
+
+        $query = DB::connection()->prepare('SELECT * FROM Band WHERE LOWER(bandname) LIKE :bandname');
         $query->execute(array('bandname' => $bandname));
-        
-        $rows = $query->fetch();
-        
+
+        $rows = $query->fetchAll();
+
         $bands = array();
-        
+
         if ($rows == null) {
             return NULL;
         }
-        
+
         foreach ($rows as $row) {
             $bands[] = new Band(array(
                 'bandname' => $row['bandname'],
@@ -78,34 +79,34 @@ class Band extends BaseModel {
                 'password' => $row['password']
             ));
         }
-        
+
         return $bands;
     }
 
     public function save() {
-        
+
         $query = DB::connection()->prepare('INSERT INTO Band (bandname, '
                 . 'description, origin, username, password) VALUES (:bandname, '
                 . ':description, :origin, :username, :password) RETURNING id');
-        
-        $query->execute(array('bandname' => $this->bandname, 'description' => 
-            $this->description, 'origin' => $this->origin, 'username' => 
+
+        $query->execute(array('bandname' => $this->bandname, 'description' =>
+            $this->description, 'origin' => $this->origin, 'username' =>
             $this->username, 'password' => $this->password));
-        
+
         $row = $query->fetch();
-        
+
         $this->id = $row['id'];
     }
-    
+
     public static function favourites($id) {
-        
+
         $query = DB::connection()->prepare('SELECT * FROM Band WHERE id IN '
                 . '(SELECT favourite FROM BandFavourite WHERE band_id = :id)');
         $query->execute(array('id' => $id));
-        
+
         $rows = $query->fetchAll();
         $favourites = array();
-        
+
         foreach ($rows as $row) {
             $favourites[] = new Band(array(
                 'bandname' => $row['bandname'],
@@ -116,7 +117,8 @@ class Band extends BaseModel {
                 'password' => $row['password']
             ));
         }
-        
+
         return $favourites;
     }
+
 }
